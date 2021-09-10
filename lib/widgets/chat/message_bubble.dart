05047ccc915a2dc30774e8dfe9601afe581a4f20
M Shashanka <shashanka318@gmail.com>
2021-09-10 19:25:36 +0530

    display username

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget{
-  final String message;
+  final String message, username;
   final bool isMe;
   final Key key;
-  MessageBubble(this.message, this.isMe, this.key);
+  MessageBubble(this.message, this.username, this.isMe, this.key);
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -23,10 +23,24 @@ class MessageBubble extends StatelessWidget{
           width: 140,
           padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
           margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-          child: Text(message, 
-            style: TextStyle(
-              color: isMe ? Colors.black : Theme.of(context).accentTextTheme.headline1.color),
-            ),
+          child: Column(
+            crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: [
+              Text(
+                username,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: isMe ? Colors.black : Theme.of(context).accentTextTheme.headline1.color,
+                ),
+              ),
+              Text(message, 
+                style: TextStyle(
+                  color: isMe ? Colors.black : Theme.of(context).accentTextTheme.headline1.color
+                ),
+                textAlign: isMe ? TextAlign.end : TextAlign.start,
+              ),
+            ],
+          ),
         ),
       ],
     );
