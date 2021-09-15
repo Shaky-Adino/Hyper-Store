043724a4183930350eb4ea39be3b915a3ae9e562
M Shashanka <shashanka318@gmail.com>
2021-09-15 18:00:00 +0530

    add option for editing profile and changing password

--- lib/screens/account.dart
+++ lib/screens/account.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import './user_profile.dart';
 import '../providers/auth.dart';
 import '../widgets/app_drawer.dart';
 
@@ -8,6 +9,7 @@ class Account extends StatelessWidget {
   static const routeName = '/account-screen';
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     final authData = Provider.of<Auth>(context);
     String username = authData.username;
     String url = authData.userImage;
@@ -18,76 +20,130 @@ class Account extends StatelessWidget {
       ),
       drawer: AppDrawer(),
       body: Container(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.only(left: 8, top: 8, bottom: 8),
         child: SingleChildScrollView(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               Text('User Profile'),
               SizedBox(height: 8),
-              Card(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        children: [
-                          ListTile(
-                            visualDensity: VisualDensity(horizontal: 0, vertical: -4),
-                            horizontalTitleGap: 5,
-                            leading: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('Username:'),
-                              ],
-                            ),
-                            title: username == null ? Text('Loading...') : Padding(
-                              padding: const EdgeInsets.only(bottom: 2),
-                              child: Text(username),
-                            ),
-                          ),
-                          Divider(height: 1, thickness: 1,endIndent: 5,indent: 15,),
-                          ListTile(
-                            visualDensity: VisualDensity(horizontal: 0, vertical: -4),
-                            horizontalTitleGap: 5,
-                            leading: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('Email:'),
-                              ],
-                            ),
-                            title: username == null ? Text('Loading...') : Padding(
-                              padding: const EdgeInsets.only(bottom: 2),
-                              child: Text(email),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 5),
-                      child: CircleAvatar(
-                        radius: 35,
-                        backgroundImage: url == null ? AssetImage('assets/images/profile_pic.png') : null,
-                        child: url != null ? 
-                          CachedNetworkImage(
-                            imageUrl: url,
-                            imageBuilder: (context, imageProvider) => Container(
-                              width: 70.0,
-                              height: 70.0,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  image: imageProvider, fit: BoxFit.cover),
+              Padding(
+                padding: const EdgeInsets.only(right: 8),
+                child: Card(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            ListTile(
+                              visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+                              horizontalTitleGap: 5,
+                              leading: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text('Username:'),
+                                ],
+                              ),
+                              title: username == null ? Text('Loading...') : Padding(
+                                padding: const EdgeInsets.only(bottom: 2),
+                                child: Text(username),
                               ),
                             ),
-                            progressIndicatorBuilder: (context, url, downloadProgress) => 
-                                  Center(child: CircularProgressIndicator(value: downloadProgress.progress)),
-                            fit: BoxFit.cover,
-                          ) : null,
+                            Divider(height: 1, thickness: 1,endIndent: 5,indent: 15,),
+                            ListTile(
+                              visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+                              horizontalTitleGap: 5,
+                              leading: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text('Email:'),
+                                ],
+                              ),
+                              title: username == null ? Text('Loading...') : Padding(
+                                padding: const EdgeInsets.only(bottom: 2),
+                                child: Text(email),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 5),
+                        child: CircleAvatar(
+                          radius: 35,
+                          backgroundImage: url == null ? AssetImage('assets/images/profile_pic.png') : null,
+                          child: url != null ? 
+                            CachedNetworkImage(
+                              imageUrl: url,
+                              imageBuilder: (context, imageProvider) => Container(
+                                width: 70.0,
+                                height: 70.0,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    image: imageProvider, fit: BoxFit.cover),
+                                ),
+                              ),
+                              progressIndicatorBuilder: (context, url, downloadProgress) => 
+                                    Center(child: CircularProgressIndicator(value: downloadProgress.progress)),
+                              fit: BoxFit.cover,
+                            ) : null,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 5),
+              GestureDetector(
+                onTap: (){
+                  Navigator.push(
+                    context, 
+                    MaterialPageRoute(builder: (context) => UserProfile())
+                  );
+                },
+                child: Container(
+                  margin: EdgeInsets.only(left: deviceSize.width*0.40),
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.only(
+                      topLeft: const Radius.circular(40),
+                      bottomLeft: const Radius.circular(40),
                     ),
-                  ],
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.account_circle),
+                      SizedBox(width: 10),
+                      Expanded(child: Text('Edit Profile')),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 8),
+              GestureDetector(
+                onTap: (){
+
+                },
+                child: Container(
+                  margin: EdgeInsets.only(left: deviceSize.width*0.50),
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      topLeft: const Radius.circular(40),
+                      bottomLeft: const Radius.circular(40),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.vpn_key, color: Colors.yellow),
+                      SizedBox(width: 10),
+                      Expanded(child: Text('Change Password', style: TextStyle(color: Colors.yellow))),
+                    ],
+                  ),
                 ),
               ),
             ],
--- lib/screens/user_profile.dart
+++ lib/screens/user_profile.dart
@@ -1,121 +1,121 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:cached_network_image/cached_network_image.dart';
-// import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
-// import 'package:provider/provider.dart';
-// import '../providers/auth.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import '../providers/auth.dart';
 
-// class UserProfile extends StatefulWidget {
+class UserProfile extends StatefulWidget {
 
-//   @override
-//   _UserProfileState createState() => _UserProfileState();
-// }
+  @override
+  _UserProfileState createState() => _UserProfileState();
+}
 
-// class _UserProfileState extends State<UserProfile> {
-//   final GlobalKey<FormState> _formKey = GlobalKey();
-//   File _pickedImage;
-//   bool _edit = false;
+class _UserProfileState extends State<UserProfile> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  File _pickedImage;
+  bool _edit = false;
 
-//   void _pickImage() async {
-//     final picker = ImagePicker();
-//     final img = await picker.pickImage(
-//       source: ImageSource.gallery,
-//       imageQuality: 50,
-//     );
-//     final pickedImageFile = File(img.path);
-//     setState(() {
-//         _pickedImage = pickedImageFile;
-//     });
-//   }
+  void _pickImage() async {
+    final picker = ImagePicker();
+    final img = await picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 50,
+    );
+    final pickedImageFile = File(img.path);
+    setState(() {
+        _pickedImage = pickedImageFile;
+    });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     final authData = Provider.of<Auth>(context);
-//     String url = authData.userImage;
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('User Profile'),
-//         actions: [
-//           IconButton(
-//             icon: Icon(Icons.edit), 
-//             onPressed: (){
-//               setState(() {
-//                 _edit = true;
-//               });
-//             }
-//           )
-//         ],
-//       ),
-//       body: Container(
-//         padding: const EdgeInsets.all(10),
-//         child: Column(
-//           children: [
-//             CircleAvatar(
-//               radius: 60,
-//               backgroundImage: _pickedImage == null ? CachedNetworkImage(
-//                                   imageUrl: url,
-//                                   progressIndicatorBuilder: (context, url, downloadProgress) => 
-//                                         Center(child: CircularProgressIndicator(value: downloadProgress.progress)),
-//                                   fit: BoxFit.cover,
-//                                 ) : FileImage(_pickedImage),
-//             ),
+  @override
+  Widget build(BuildContext context) {
+    final authData = Provider.of<Auth>(context);
+    String url = authData.userImage;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Profile'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.edit), 
+            onPressed: (){
+              setState(() {
+                _edit = true;
+              });
+            }
+          )
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 60,
+              backgroundImage: _pickedImage == null ? CachedNetworkImage(
+                                  imageUrl: url,
+                                  progressIndicatorBuilder: (context, url, downloadProgress) => 
+                                        Center(child: CircularProgressIndicator(value: downloadProgress.progress)),
+                                  fit: BoxFit.cover,
+                                ) : FileImage(_pickedImage),
+            ),
 
-//             SizedBox(height: 5),
+            SizedBox(height: 5),
 
-//             TextButton.icon(
-//               style: TextButton.styleFrom(
-//                 primary: Colors.orange,
-//               ),
-//               onPressed: _edit ? _pickImage : null,
-//               icon: Icon(Icons.image, size: 19,),
-//               label: Text('Edit Image', overflow: TextOverflow.visible),
-//             ),
+            TextButton.icon(
+              style: TextButton.styleFrom(
+                primary: Colors.orange,
+              ),
+              onPressed: _edit ? _pickImage : null,
+              icon: Icon(Icons.image, size: 19,),
+              label: Text('Edit Image', overflow: TextOverflow.visible),
+            ),
 
-//             SizedBox(height: 10),
+            SizedBox(height: 10),
 
-//             Form(
-//               key: _formKey,
-//               child: Column(
-//                 children: [
-//                   TextFormField(
-//                     enabled: _edit,
-//                     style: TextStyle(fontSize: 14.0),
-//                     decoration: InputDecoration(
-//                       prefixIcon: Icon(
-//                                     Icons.account_circle,
-//                                     size: 18,
-//                                     color: Colors.black,
-//                                   ),
-//                       filled: true,
-//                       enabledBorder: UnderlineInputBorder(
-//                                         borderRadius: BorderRadius.circular(10),
-//                                         borderSide: BorderSide.none,
-//                                       ),
-//                       focusedBorder: OutlineInputBorder(
-//                                       borderRadius: BorderRadius.circular(10),
-//                                     ),
-//                       labelText: 'Username'
-//                     ),
-//                     validator: (value) {
-//                                   if (value.isEmpty || value.length < 3) {
-//                                       return 'username is too short!!';
-//                                   }
-//                                   if(value.contains(' ')){
-//                                     return 'no spaces allowed';
-//                                   }
-//                                   return null;
-//                               },
-//                     onSaved: (value){
-//                                 username = value;
-//                             },
-//                   ),                
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    enabled: _edit,
+                    style: TextStyle(fontSize: 14.0),
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                                    Icons.account_circle,
+                                    size: 18,
+                                    color: Colors.black,
+                                  ),
+                      filled: true,
+                      enabledBorder: UnderlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        borderSide: BorderSide.none,
+                                      ),
+                      focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                      labelText: 'Username'
+                    ),
+                    validator: (value) {
+                                  if (value.isEmpty || value.length < 3) {
+                                      return 'username is too short!!';
+                                  }
+                                  if(value.contains(' ')){
+                                    return 'no spaces allowed';
+                                  }
+                                  return null;
+                              },
+                    onSaved: (value){
+                                username = value;
+                            },
+                  ),                
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
